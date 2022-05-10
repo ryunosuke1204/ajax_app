3355aad8bb50c04c366c165e78da5888a64afc85
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-10 18:11:58 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,16 @@
-
-window.addEventListener('load',post);
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
 
 function post (){
   const submit = document.getElementById("submit");
@@ -9,10 +20,19 @@ function post (){
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
-    XHR.response = "jason";
+    XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+     const formText = document.getElementById("content")
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+    formText.value = "";
+    };
   });
 };
 
 window.addEventListener('load', post);
-
