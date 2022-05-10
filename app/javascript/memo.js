798d3f5e517fd00e17d1474cf891fbcbd9670d8d
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-10 17:11:25 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('load',post);
+
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.response = "jason";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
+
